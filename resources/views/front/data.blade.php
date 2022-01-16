f2b4419f00414d20046684c2e53affc8b3c59718
islamshu12 <islamshu12@gmail.com>
2022-01-16 11:34:37 +0200

    dd

--- resources/views/front/data.blade.php
+++ resources/views/front/data.blade.php
@@ -9,6 +9,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('css/english.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/arabic.css') }}"> --}}
+
 </head>
 <style>
     body {
